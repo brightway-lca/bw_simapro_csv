7dc8b6bbd56d6291a63b2183b35272b679393503
Chris Mutel <cmutel@gmail.com>
2024-06-06 15:30:09 +0200

    Linting

--- bw_simapro_csv/blocks/process.py
+++ bw_simapro_csv/blocks/process.py
@@ -23,7 +23,7 @@ LIST_ELEMENTS = {
     "Waste treatment",
 }
 HAS_SUBCATEGORY = {
-    "Economic issues"
+    "Economic issues",
     "Emissions to air",
     "Emissions to soil",
     "Emissions to water",
@@ -33,7 +33,7 @@ HAS_SUBCATEGORY = {
     "Social issues",
 }
 NO_SUBCATEGORY = {
-    "Avoided products",    
+    "Avoided products",
     "Electricity/heat",
     "Materials/fuels",
     "Products",
@@ -88,7 +88,7 @@ class Process(SimaProCSVBlock):
         8. comment
 
         However, sometimes the value is in index 2, and the unit in index 3. Because why not! We assume default ordering unless we find a number in index 2.
-        """        
+        """
         key = block[self.index][0]
         data = []
 
@@ -96,22 +96,24 @@ class Process(SimaProCSVBlock):
 
         while any(block[self.index]):
             if key in HAS_SUBCATEGORY:
-                data.append({
-                    'name': a,
-                    'categories': (key, b),
-                    'maybe_unit': c,
-                    'maybe_value': d,
-                    'kind': e,
-                    'field1': f,
-                    'field2': g,
-                    'field3': h
-                    } for (a,b,c,d,e,f,g,h) in block[self.index]
+                data.append(
+                    {
+                        "name": a,
+                        "categories": (key, b),
+                        "maybe_unit": c,
+                        "maybe_value": d,
+                        "kind": e,
+                        "field1": f,
+                        "field2": g,
+                        "field3": h,
+                    }
+                    for (a, b, c, d, e, f, g, h) in block[self.index]
                 )
             self.index += 1
 
         # Skip empty line ending this section
         self.index += 1
-        
+
         return key, data
 
     def resolve_unit_amount(self, a: str, b: str, key: str) -> dict:
@@ -129,9 +131,9 @@ class Process(SimaProCSVBlock):
             logger.warning("Ambiguous unit/value pair: '{a}' and '{b}' in section {key}")
             unit, amount = a, b
         if has_letters.search(amount):
-            return {'unit': unit, 'formula': amount}
+            return {"unit": unit, "formula": amount}
         # TBD: Evaulate number
-        return {'unit': unit, 'amount': float(amount)}
+        return {"unit": unit, "amount": float(amount)}
 
     def pull_metadata_pair(self, block: List[list], header: dict) -> (str, str):
         key = block[self.index][0]
