14f216c253675b26dbc65efb1f9e144a4eb0a809
Chris Mutel <cmutel@gmail.com>
2024-06-13 11:34:42 +0200

    Use temp directory for logs during test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Fixtures for bw_simapro_csv"""
 
 from pathlib import Path
+import platformdirs
 
 import pytest
 
@@ -10,3 +11,11 @@ FIXTURES_DIR = Path(__file__).parent / "fixtures"
 @pytest.fixture
 def fixtures_dir():
     return FIXTURES_DIR
+
+
+@pytest.fixture(autouse=True)
+def temporary_logs_dir(monkeypatch, tmp_path):
+    def temp_user_log_dir(*args, **kwargs):
+        return tmp_path
+
+    monkeypatch.setattr(platformdirs, "user_log_dir", temp_user_log_dir)
